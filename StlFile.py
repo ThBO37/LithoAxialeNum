d500e4451063be7cd24e6ccbbffed977bcbc0b3f
ThBO37 <31624894+ThBO37@users.noreply.github.com>
2020-12-15 20:42:27 +0100

    Update StlFile.py

--- StlFile.py
+++ StlFile.py
@@ -36,7 +36,11 @@ class StlFile:
             return text
 
     def vertices(self):
-        return np.unique(self.data(), axis = 0)
+        array = np.unique(self.data(), axis = 0)
+        array1 = np.empty((np.shape(array))[0], dtype=Point)
+        for i in range(np.shape(array)[0]):
+            array1[i] = Point(array[i,0], array[i,1], array[i,2])
+        return array1
 
     def verticesNumber(self):
         return np.shape(self.vertices())[0]
@@ -45,10 +49,43 @@ class StlFile:
         return (np.shape(self.data())[0])//3
 
     def faces(self):
-        array = np.zeros((self.facesNumber(), 3), dtype=np.ndarray)
+        array = np.empty(self.facesNumber(), dtype=Face)
         data = self.data()
         for i in range(np.shape(array)[0]):
-            array[i, 0] = data[i]
-            array[i, 1] = data[i+1]
-            array[i, 2] = data[i+2]
+            Point1 = Point(data[i,0],data[i,1],data[i,2])
+            Point2 = Point(data[i+1,0],data[i+1,1],data[i+1,2])
+            Point3 = Point(data[i+2,0],data[i+2,1],data[i+2,2])
+            array[i] = Face(Point1, Point2, Point3)
         return array
+
+class Point:
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
+
+    def coordinates(self):
+        return self.x, self.y, self.z
+
+class Vector:
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
+
+class Segment:
+    def __init__(self, Point1, Point2):
+        self.Point1 = Point1
+        self.Point2 = Point2
+
+class Face:
+    def __init__(self, Point1, Point2, Point3):
+        self.Point1 = Point1
+        self.Point2 = Point2
+        self.Point3 = Point3
+
+    def points(self):
+        return self.Point1, self.Point2, self.Point3
+
+    def coordinates(self):
+        return self.Point1.coordinates(), self.Point2.coordinates(), self.Point3.coordinates()
