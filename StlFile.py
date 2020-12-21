6c91ad08132b9d4b34390d174e44f164dec2d01a
ThBO37 <31624894+ThBO37@users.noreply.github.com>
2020-12-21 22:23:07 +0100

    Update StlFile.py

--- StlFile.py
+++ StlFile.py
@@ -1,166 +1,65 @@
-import os
 import numpy as np
-import math
+import matplotlib as mpl
+from mpl_toolkits.mplot3d import Axes3D
+from mpl_toolkits.mplot3d.art3d import Poly3DCollection
+import matplotlib.pyplot as plt
+import os
 
-## @TimSC - line-plane-collision.py
+os.chdir('d:\\desktop\\tipe\\code\\radon transform')
 
 class Mesh:
     def __init__(self, path):
         self.path = path
 
-    def format(self):
-        textchars = bytearray(
-            {7, 8, 9, 10, 12, 13, 27} | set(range(0x20, 0x100)) - {0x7F}
-        )
-        is_binary_string = lambda bytes: bool(bytes.translate(None, textchars))
-        if is_binary_string(open(self.path, "rb").read(1024)):
-            return "Binary"
-        else:
-            return "Ascii"
-
-    def text(self):
-        if self.format() == "Binary":
-            return open(self.path, "rb").read()
-        else:
-            return open(self.path, "r").read()
-
-    def data(self):
-        text = self.text()
-        if self.format() == "Ascii":
-            list = text.split('\n')
-            list = [x for x in list if 'vertex' in x]
-            list = [x.split(' ') for x in list]
-            for i in range(len(list)):
-                list[i]=[float(x) for x in list[i] if '.' in x]
-            array = np.asarray(list)
-            return array
-        else:
-            return text
-
-    def vertices(self):
-        array = np.unique(self.data(), axis = 0)
-        array1 = np.empty((np.shape(array))[0], dtype=Point)
-        for i in range(np.shape(array)[0]):
-            array1[i] = Point(array[i,0], array[i,1], array[i,2])
-        return array1
-
-    def verticesNumber(self):
-        return np.shape(self.vertices())[0]
-
-    def facesNumber(self):
-        return (np.shape(self.data())[0])//3
-
-    def faces(self):
-        array = np.empty(self.facesNumber(), dtype=Face)
-        data = self.data()
-        for i in range(np.shape(array)[0]):
-            Point1 = Point(data[i,0],data[i,1],data[i,2])
-            Point2 = Point(data[i+1,0],data[i+1,1],data[i+1,2])
-            Point3 = Point(data[i+2,0],data[i+2,1],data[i+2,2])
-            array[i] = Face(Point1, Point2, Point3)
+    def raw(self):
+        text = open(self.path, "r").read()
+        list = text.split('\n')
+        list = [x for x in list if 'vertex' in x]
+        list = [x.split(' ') for x in list]
+        for i in range(len(list)):
+            list[i]=[float(x) for x in list[i] if '.' in x]
+        array = np.asarray(list)
         return array
 
+    def points(self):
+        raw = np.unique(self.raw(), axis=0)
+        return np.array([Point(i[0],i[1],i[2]) for i in raw])
+
+    def triangles(self):
+        raw = self.raw()
+        points = np.array([Point(i[0],i[1],i[2]) for i in raw])
+        return np.array([Triangle(points[3*i], points[3*i+1], points[3*i+2]) for i in range(np.shape(points)[0]//3)])
+
+    def edges(self):
+        triangles = self.triangles()
+        list = []
+        for i in triangles:
+            list.append(Line(i.Point1, i.Point2))
+            list.append(Line(i.Point2, i.Point3))
+            list.append(Line(i.Point1, i.Point3))
+        return np.asarray(list)
+
+    def show(self):
+        t = self.triangles()
+        triangles = [((i.Point1.x, i.Point1.y, i.Point1.z), (i.Point2.x, i.Point2.y, i.Point2.z), (i.Point3.x, i.Point3.y, i.Point3.z)) for i in t]
+        fig = plt.figure()
+        ax = fig.gca(projection='3d')
+        ax.add_collection(Poly3DCollection(triangles, facecolors='w', edgecolors='black', linewidths=1))
+        plt.show()
+
 class Point:
-    def __init__(self, x, y, z):
+    def __init__(self, x: float, y: float, z: float):
         self.x = x
         self.y = y
         self.z = z
 
-    def coordinates(self):
-        array = np.zeros(3, dtype=float)
-        array[0] = self.x
-        array[1] = self.y
-        array[2] = self.z
-        return array
-
-    def isInsideFace(self, face):
-        Point1 = face.points()[0]
-        Point2 = face.points()[1]
-        Point3 = face.points()[2]
-        Triangle0 = Face(Point1, Point2, Point3)
-        Triangle1 = Face(self, Point1, Point2)
-        Triangle2 = Face(self, Point1, Point3)
-        Triangle3 = Face(self, Point2, Point3)
-        return round(Triangle0.area(), 8) ==  round(Triangle1.area()+Triangle2.area()+Triangle3.area(), 8)
-    
 class Line:
-    def __init__(self, Point1, Point2):
+    def __init__(self, Point1: Point, Point2: Point):
         self.Point1 = Point1
         self.Point2 = Point2
 
-    def vector(self):
-        Coor1 = self.Point1.coordinates()
-        Coor2 = self.Point2.coordinates()
-        x = Coor2[0]-Coor1[0]
-        y = Coor2[1]-Coor1[1]
-        z = Coor2[2]-Coor1[2]
-        return np.array([x, y, z])
-
-    def point(self):
-        return self.Point1
-        
-class Face:
-    def __init__(self, Point1, Point2, Point3):
+class Triangle:
+    def __init__(self, Point1: Point, Point2: Point, Point3: Point):
         self.Point1 = Point1
         self.Point2 = Point2
         self.Point3 = Point3
-
-    def points(self):
-        array = np.zeros(3, dtype=Point)
-        array[0] = self.Point1
-        array[1] = self.Point2
-        array[2] = self.Point3
-        return array 
-
-    def coordinates(self):
-        array = np.zeros((3,3), dtype=float)
-        array[0] = self.Point1.coordinates()
-        array[1] = self.Point2.coordinates()
-        array[2] = self.Point3.coordinates()
-        return array
-
-    def edges(self):
-        array = np.zeros(3, dtype=Line)
-        array[0] = Line(self.Point1, self.Point2)
-        array[1] = Line(self.Point2, self.Point3)
-        array[2] = Line(self.Point3, self.Point1)
-        return array
-
-    def area(self):
-        vec1 = self.edges()[1].vector()
-        vec2 = self.edges()[2].vector()
-        area = 0.5*np.linalg.norm(np.cross(vec1, vec2))
-        return area
-
-    def isValid(self):
-        return (self.area() != 0.0)
-
-    def point(self):
-        return self.Point1
-
-    def normal(self):
-        vec1 = self.edges()[1].vector()
-        vec2 = self.edges()[2].vector()
-        return np.cross(vec1, vec2)
-
-    def lineIntersection(self, Line):
-        planeNormal = self.normal()
-        planePoint = self.point().coordinates()
-        rayDirection = Line.vector()
-        rayPoint = Line.point().coordinates()
-        epsilon=1e-6
-
-        ndotu = planeNormal.dot(rayDirection)
-        if abs(ndotu) < epsilon:
-            raise RuntimeError("no intersection or line is within plane")
-
-        w = rayPoint - planePoint
-        si = -planeNormal.dot(w) / ndotu
-        Psi = w + si * rayDirection + planePoint
-
-        Intersection = Point(Psi[0], Psi[1], Psi[2])
-        return Intersection
-
-    def intersectionInFace(self, Line):
-        interPoint = self.lineIntersection(Line)
-        return interPoint.isInsideFace(self)
